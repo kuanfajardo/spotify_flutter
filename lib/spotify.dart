617e9fc11d8f708dfc38401810784c74f2489635
Juan Diego Fajardo <jfajardo@mit.edu>
2019-06-23 20:47:45 +0200

    Add all class skeletons and enums for iOS.

--- lib/spotify.dart
+++ lib/spotify.dart
@@ -11,3 +11,62 @@ class Spotify {
     return version;
   }
 }
+
+enum SpotifyLogLevel { none, debug, info, error, }
+
+enum SpotifyConnectionParamsImageFormat { any, jpeg, png, }
+
+/// See https://developer.spotify.com/web-api/using-scopes/
+enum SpotifyScope {
+  playlistReadPrivate,
+  playlistReadCollaborative,
+  playlistModifyPublic,
+  playlistModifyPrivate,
+  userFollowRead,
+  userFollowModify,
+  userLibraryRead,
+  userLibraryModify,
+  userReadBirthDate,
+  userReadEmail,
+  userReadPrivate,
+  userTopRead,
+  ugcImageUpload,
+  streaming,
+  appRemoteControl,
+  userReadPlaybackState,
+  userModifyPlaybackState,
+  userReadCurrentlyPlaying,
+  userReadRecentlyPlayed,
+}
+
+enum SpotifyAuthorizationOptions { default_, client, }
+
+enum SpotifyContentType { default_, navigation, fitness, }
+
+enum SpotifyPlaybackOptionsRepeatMode { off, track, context, }
+
+class SpotifyConfiguration {}
+class SpotifyConnectionParams {}
+// SpotifyDelegate
+class SpotifyPlayerAPI {}
+class SpotifyImageAPI {}
+class SpotifyUserAPI {}
+class SpotifyContentAPI {}
+class SpotifyError {}
+class SpotifySession {}
+class SpotifySessionManager {}
+// SpotifySessionManagerDelegate
+class SpotifyAlbum {}
+class SpotifyArtist {}
+class SpotifyContentItem {}
+class SpotifyCrossfadeState {}
+class SpotifyImageRepresentable {}
+class SpotifyLibraryState {}
+class SpotifyPlaybackOptions {}
+class SpotifyPlaybackRestrictions {}
+class SpotifyPlayerState {}
+// SpotifyPlayerStateDelegate
+class SpotifyPodcastPlaybackSpeed {}
+class SpotifyTrack {}
+class SpotifyUserAPIDelegate {}
+class SpotifyUserCapabilities {}
