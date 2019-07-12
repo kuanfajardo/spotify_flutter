a1f6e36d1044014e1f6fdc35d9a2ee7182aee801
Juan Diego Fajardo <jfajardo@mit.edu>
2019-07-13 01:14:52 +0200

    Refactor API directory structure.

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,6 +1,6 @@
 library api;
 
-export 'package:spotify/api/lib/contentApi.dart';
-export 'package:spotify/api/lib/imageApi.dart';
-export 'package:spotify/api/lib/playerApi.dart';
-export 'package:spotify/api/lib/userApi.dart';
+export 'package:spotify/api/lib/content/contentApi.dart';
+export 'package:spotify/api/lib/image/imageApi.dart';
+export 'package:spotify/api/lib/player/playerApi.dart';
+export 'package:spotify/api/lib/user/userApi.dart';
